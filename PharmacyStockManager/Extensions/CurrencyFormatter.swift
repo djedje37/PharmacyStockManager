06da34bd1b618ebedb28affe981c6d2a991e698c
Djeneba KANE <d.kane@vogo.fr>
2026-07-24 22:03:08 +0200

    Add CurrencyFormatter extension

---
+++ PharmacyStockManager/Extensions/CurrencyFormatter.swift
@@ -0,0 +1,25 @@
+//
+//  CurrencyFormatter.swift
+//  PharmacyStockManager
+//
+//  Created by Djeneba KANE on 18/07/2026.
+//
+
+import Foundation
+extension Double {
+   
+   private static let fcfaToEuroRate = 655.957
+
+   // TODO: currency and conversion in relief of locale zone 
+   var fcfaToEuro: Double {
+      self / Self.fcfaToEuroRate
+   }
+   
+    var euroFormatted: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencyCode = "EUR"
+        formatter.locale = Locale(identifier: "fr_FR")
+        return formatter.string(from: NSNumber(value: self)) ?? "\(self)"
+    }
+}
